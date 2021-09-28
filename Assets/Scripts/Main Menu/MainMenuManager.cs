561a68010ee24e325fd5fac1b78c89b059e252b9
Josh <joshmh7128@gmail.com>
2021-09-28 10:12:36 -0400

    main menu basics

---
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+    // our main menu UI pieces
+    [SerializeField] Button playButton; // main play button
+    [SerializeField] Button optionsButton; // options button 
+    [SerializeField] Button exitButton; // exit game button
+    [SerializeField] GameObject optionsPanel; // options panel where we store our options controls
+    [SerializeField] Button toggleFullscreenButton; // toggle fullscreen
+    [SerializeField] Slider masterVolumeSlider; // master slider
+    [SerializeField] Slider effectsVolumeSlider; // effects slider
+    [SerializeField] Slider musicVolumeSlider; // music slider
+    float masterVolume; // master volume
+    float effectsVolume; // effects volume
+    float musicVolume; // music volume
+    [SerializeField] AudioMixer masterMixer;
+
+    // show and hide our options panel
+    public void ToggleOptions()
+    { optionsPanel.SetActive(!optionsPanel.activeInHierarchy); }
+
+    // toggle fullscreen
+    public void ToggleFullscreen()
+    { Screen.fullScreen = !Screen.fullScreen; }
+
+    // load in to our primer
+    public void AdvanceToPrimer()
+    { SceneManager.LoadScene("Primer", LoadSceneMode.Single); }
+
+    // FixedUpdate is called once per frame
+    void FixedUpdate()
+    {
+        // make sure to set our volumes
+        masterMixer.SetFloat("masterVolume", masterVolumeSlider.value);
+        masterMixer.SetFloat("sfxVolume", effectsVolumeSlider.value);
+        masterMixer.SetFloat("musicVolume", musicVolumeSlider.value);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
