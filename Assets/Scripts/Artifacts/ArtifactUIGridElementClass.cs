cacb86a8eb57cfa55b25a6ec081f02953880b64c
Josh <joshmh7128@gmail.com>
2021-10-17 21:12:18 -0400

    Icon List

--- Assets/Scripts/Artifacts/ArtifactUIGridElementClass.cs
+++ Assets/Scripts/Artifacts/ArtifactUIGridElementClass.cs
@@ -11,6 +11,36 @@ public class ArtifactUIGridElementClass : MonoBehaviour
     public Image artifactIcon;
     public Text selectionTitleText;
     public Text selectionInfoText;
+    [SerializeField] Button ourButton;
+
+    private void Start()
+    {
+        if (artifactTitleText != null)
+        {
+            if (artifactTitleText.ToString().Contains("Filler"))
+            {
+
+            }
+
+            if (artifactTitleText.ToString().Contains("Uncommon"))
+            {
+                ColorBlock uncommonColors = new ColorBlock();
+                uncommonColors.normalColor = new Color(136, 255, 136, 255);
+                uncommonColors.selectedColor = new Color(130, 226, 129, 255);
+                uncommonColors.pressedColor = new Color(104, 195, 104, 255);
+                ourButton.colors = uncommonColors;
+            }
+
+            if (artifactTitleText.ToString().Contains("Rare"))
+            {
+                ColorBlock rareColors = new ColorBlock();
+                rareColors.normalColor = new Color(121, 199, 255, 255);
+                rareColors.selectedColor = new Color(117, 179, 231, 255);
+                rareColors.pressedColor = new Color(85, 135, 231, 255);
+                ourButton.colors = rareColors;
+            }
+        }
+    }
 
     public void LocalOnClick()
     {
--- Assets/Scripts/Player/InventoryArtifactUIHandler.cs
+++ Assets/Scripts/Player/InventoryArtifactUIHandler.cs
@@ -7,10 +7,13 @@ public class InventoryArtifactUIHandler : MonoBehaviour
 {
     // our prefab object
     [SerializeField] GameObject artifactUIGridElementPrefab;
+    [SerializeField] GameObject artifactIconUIGridPrefab;
     // our grid parent object
     [SerializeField] Transform gridParent;
+    [SerializeField] Transform iconGridParent;
     // all the objects in that grid object
     public List<ArtifactUIGridElementClass> artifactUIGridElementClasses;
+    public List<GameObject> artifactMiniIcons;
     // our UI elements
     public Text selectedTitleText;
     public Text selectedInfoText;
@@ -21,6 +24,7 @@ public class InventoryArtifactUIHandler : MonoBehaviour
         // make our object and add it to the grid
         ArtifactUIGridElementClass newElement;
         newElement = Instantiate(artifactUIGridElementPrefab, gridParent).GetComponent<ArtifactUIGridElementClass>();
+        
         // make sure the information on our newElement is correct
         newElement.gameObject.transform.SetAsFirstSibling();
         newElement.artifactIcon.sprite = icon;
@@ -30,11 +34,21 @@ public class InventoryArtifactUIHandler : MonoBehaviour
         newElement.selectionInfoText = selectedInfoText;
         // add it to our list
         artifactUIGridElementClasses.Add(newElement);
+        
+        // add our icon
+        GameObject artifactMiniIcon;
+        artifactMiniIcon = Instantiate(artifactIconUIGridPrefab, iconGridParent);
+        artifactMiniIcon.GetComponent<Image>().sprite = icon;
     }
 
     public void ClearInventoryGrid()
     {
         foreach (ArtifactUIGridElementClass element in artifactUIGridElementClasses)
+        {
+            Destroy(element.gameObject);
+        }       
+        
+        foreach (GameObject element in artifactMiniIcons)
         {
             Destroy(element.gameObject);
         }
