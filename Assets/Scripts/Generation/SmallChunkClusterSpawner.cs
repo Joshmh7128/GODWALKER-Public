11cef5185024d59efac46aa8f6d4d3b8dde9437c
Josh <joshmh7128@gmail.com>
2021-10-24 10:38:02 -0400

    Updating Desert 001

--- Assets/Scripts/Generation/SmallChunkClusterSpawner.cs
+++ Assets/Scripts/Generation/SmallChunkClusterSpawner.cs
@@ -14,7 +14,7 @@ public class SmallChunkClusterSpawner : MonoBehaviour
         // choose a random amount of small chunks then spawn them in
         for (float i = 0; i < dropAmount; i++)
         {
-            Instantiate(smallChunk, transform.position + new Vector3(Random.Range(-3, 3), transform.position.y + 1, Random.Range(-3, 3)), Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+            Instantiate(smallChunk, transform.position + new Vector3(Random.Range(-3, 3), 1, Random.Range(-3, 3)), Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
         }
     }
 }
