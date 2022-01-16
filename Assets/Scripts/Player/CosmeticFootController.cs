4f9f5fa45ddf3a57027e78e0db14492d57f1a29d
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-01-15 19:38:40 -0500

    feet no work

--- Assets/Scripts/Player/CosmeticFootController.cs
+++ Assets/Scripts/Player/CosmeticFootController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CosmeticFootController : MonoBehaviour
 {
-    [SerializeField] Transform hipTransform, kneeTransform; // we will get our hips rotation and apply it to ourselves. when our foot is not on the ground, we can use the rotation of our knees to simulate running motion
+    [SerializeField] Transform hipTransform, lookTarget; // we will get our hips rotation and apply it to ourselves. when our foot is not on the ground, we can use the rotation of our knees to simulate running motion
+    
     Quaternion rot; RaycastHit hit; // everything to do with our firing and recording of the ground normal vector
+    Vector3 upNormDif;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -13,16 +15,13 @@ public class CosmeticFootController : MonoBehaviour
         // if we are on the ground, use the ground for our X rotation and our hips for our Y rotation
         if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.5f))
         {
-            // our up is hit.normal our forward is the direction our hips are facing
-           
-            rot.eulerAngles = new Vector3(0f,hit.normal.y,hipTransform.forward.z);
+            transform.LookAt(hit.normal);
+            lookTarget.position = hipTransform.forward * 2;
         }
-
-        transform.rotation = rot;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, transform.position+Vector3.down*0.5f);
+        // Gizmos.DrawLine(transform.position, newDirection);
     }
 }
