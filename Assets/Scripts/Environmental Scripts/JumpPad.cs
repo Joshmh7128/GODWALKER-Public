4d5c8b876d145f2c16ecfe5b8c2c11ebcb470f04
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-02-12 15:10:56 -0500

    Had a minor lighting issue, appears to be fixed now

--- Assets/Scripts/Environmental Scripts/JumpPad.cs
+++ Assets/Scripts/Environmental Scripts/JumpPad.cs
@@ -9,4 +9,17 @@ public class JumpPad : MonoBehaviour
     /// 
 
     [SerializeField] float jumpPower;
+    [SerializeField] AudioSource audioSource;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if our trigger comes into contact with the player
+        if (other.transform.tag == "Player")
+        {   // launch them
+            UpgradeSingleton.Instance.player.JumpLaunch(jumpPower);
+            // play our sound
+            if (audioSource)
+            audioSource.Play();
+        }
+    }
 }
