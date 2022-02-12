4ba29b1cc6cde5e05804250881c40bd36d8a99c2
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-02-12 14:59:51 -0500

    Build model, adding script functionality

---
+++ Assets/Scripts/Environmental Scripts/JumpPad.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    /// 
+    /// class exists to work with jump pads
+    /// 
+
+    [SerializeField] float jumpPower;
+}
