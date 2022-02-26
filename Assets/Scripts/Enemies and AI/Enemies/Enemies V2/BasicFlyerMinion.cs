c8649e9ebe552a7bff64449f1c07dd851be5fa34
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-02-26 11:57:03 -0500

    Starting Scripting

---
+++ Assets/Scripts/Enemies and AI/Enemies/Enemies V2/BasicFlyerMinion.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicFlyerMinion : EnemyClass
+{
+    ///
+    /// this script will manage our basic flying enemy
+    /// We want this enemy to move around relatively slowly in the air,
+    /// and fire more frequently than our basic tread minion
+    /// 
+
+    public override void TakeDamage(int dmg)
+    {
+        throw new System.NotImplementedException();
+    }
+}
