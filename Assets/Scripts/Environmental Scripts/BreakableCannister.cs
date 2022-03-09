69b3d55a5ef5541582b5b78ca46a2e0f99493864
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-03-09 16:37:16 -0500

    Working on pickup cannisters

---
+++ Assets/Scripts/Environmental Scripts/BreakableCannister.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableCannister : MonoBehaviour
+{
+    /// 
+    /// script manages the breakable cannister object
+    /// 
+
+    // main variables
+    [SerializeField] float dropAmount; // how much of our drop should we drop?
+    [SerializeField] GameObject dropObject; // what object are we dropping?
+
+}
