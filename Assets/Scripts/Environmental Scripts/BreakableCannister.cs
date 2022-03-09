b614cee02e5f2137c4cfa2c663cb82cfddbce193
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-03-09 17:53:58 -0500

    Both cannisters done, adding them to the environments

--- Assets/Scripts/Environmental Scripts/BreakableCannister.cs
+++ Assets/Scripts/Environmental Scripts/BreakableCannister.cs
@@ -10,6 +10,29 @@ public class BreakableCannister : MonoBehaviour
 
     // main variables
     [SerializeField] float dropAmount; // how much of our drop should we drop?
-    [SerializeField] GameObject dropObject; // what object are we dropping?
+    [SerializeField] GameObject dropObject, breakParticle; // what object are we dropping?
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            // run our destruction function
+            CustomDestroy();
+        }
+    }
+
+    private void CustomDestroy()
+    {
+        // drop our stuff
+        for (float i = dropAmount; i > 0; i--)
+        {
+            Instantiate(dropObject, transform.position, Quaternion.identity, null);
+        }
+        // spawn our break particle
+        Instantiate(breakParticle, transform.position, Quaternion.identity, null);
+        // shake the players screen
+        Camera.main.GetComponent<CameraScript>().SnapScreenShake(1f);
+        // destroy ourselves
+        Destroy(gameObject);
+    }
 }
