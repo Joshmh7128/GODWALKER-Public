b2471973799284e6425081bbefb0e8c680fb671b
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-03-10 10:22:54 -0500

    Reworking cannisters to add resources on trigger collision instead of drop resource bubbles

--- Assets/Scripts/Environmental Scripts/BreakableCannister.cs
+++ Assets/Scripts/Environmental Scripts/BreakableCannister.cs
@@ -11,6 +11,9 @@ public class BreakableCannister : MonoBehaviour
     // main variables
     [SerializeField] float dropAmount; // how much of our drop should we drop?
     [SerializeField] GameObject dropObject, breakParticle; // what object are we dropping?
+    [SerializeField] bool dropsObject; // do we drop an object?
+    [SerializeField] enum dropTypes { health, power, nanites}; // what kinds of things can we drop?
+    [SerializeField] dropTypes dropType; // what is our droptype?
 
     private void OnTriggerEnter(Collider other)
     {
@@ -23,10 +26,29 @@ public class BreakableCannister : MonoBehaviour
 
     private void CustomDestroy()
     {
-        // drop our stuff
-        for (float i = dropAmount; i > 0; i--)
+        if (dropsObject)
         {
-            Instantiate(dropObject, transform.position, Quaternion.identity, null);
+            // drop our stuff
+            for (float i = dropAmount; i > 0; i--)
+            {
+                Instantiate(dropObject, transform.position, Quaternion.identity, null);
+            }
+        } else
+        {
+            // depending on what we drop, give the player the selected values
+            switch (dropType)
+            {
+                case dropTypes.health:
+                    UpgradeSingleton.Instance.player.AddHP((int)dropAmount);
+                    break;
+
+                case dropTypes.nanites:
+                    break;
+
+                case dropTypes.power:
+                    UpgradeSingleton.Instance.player.powerAmount += dropAmount;
+                    break;
+            }
         }
         // spawn our break particle
         Instantiate(breakParticle, transform.position, Quaternion.identity, null);
