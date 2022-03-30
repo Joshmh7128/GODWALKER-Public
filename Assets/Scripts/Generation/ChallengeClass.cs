273132a7cb97617eb5a7b132d333a56993ec380b
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-03-30 19:18:56 -0400

    Building sample challenge

---
+++ Assets/Scripts/Generation/ChallengeClass.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeClass : MonoBehaviour
+{
+   // class exists for other challenges to inherit from
+}
