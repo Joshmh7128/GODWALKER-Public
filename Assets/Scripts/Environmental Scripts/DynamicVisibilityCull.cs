13aff1f8f0c54dee908c57070d2140223a740413
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-04-24 18:46:39 -0400

    experimenting with dynamic occlusion

---
+++ Assets/Scripts/Environmental Scripts/DynamicVisibilityCull.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicVisibilityCull : MonoBehaviour
+{
+    Renderer localRenderer; // our renderer
+
+    private void Start()
+    {
+        // get our renderer
+        localRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void OnBecameVisible()
+    {
+        localRenderer.enabled = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        localRenderer.enabled = true;
+    }
+}
