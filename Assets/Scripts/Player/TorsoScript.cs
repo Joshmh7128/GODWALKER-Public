05d5ea9129de1b7ec9b6c57b133f40ca89ce2e6e
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-07-23 10:09:33 -0400

    reorienting project root to new version

--- Assets/Scripts/Player/TorsoScript.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TorsoScript : MonoBehaviour
-{
-    [SerializeField] Transform neckTransform, hipTransform, shoulderRight, shoulderLeft; // pre-defined positions
-}
