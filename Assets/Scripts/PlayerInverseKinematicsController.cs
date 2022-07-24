b4eae05c6c44117ffbf9b83ce3e2cec191250f85
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-07-24 13:42:15 -0400

    Working on character turning in place

--- Assets/Scripts/PlayerInverseKinematicsController.cs
+++ Assets/Scripts/PlayerInverseKinematicsController.cs
@@ -9,10 +9,33 @@ public class PlayerInverseKinematicsController : MonoBehaviour
     [SerializeField] Transform targetRightHand, targetLeftHand, targetLook;
     [SerializeField] Animator animator;
 
+    // control
+    [SerializeField] bool rightArm, leftArm, look;
+
     private void OnAnimatorIK(int layerIndex)
     {
-        // right hands
-        animator.SetIKPosition(AvatarIKGoal.RightHand, targetRightHand.position);
-        animator.SetIKRotation(AvatarIKGoal.RightHand, targetRightHand.rotation);
+
+        if (rightArm)
+        {   
+            // right hands
+            animator.SetIKPosition(AvatarIKGoal.RightHand, targetRightHand.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, targetRightHand.rotation);
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+        }
+
+        if (leftArm)
+        {
+            // Left hands
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, targetLeftHand.position);
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, targetLeftHand.rotation);
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+        }
+
+        if (look)
+        {
+            // look target
+            animator.SetLookAtPosition(targetLook.position);
+            animator.SetLookAtWeight(1, 1, 1, 1, 0);
+        }
     }
 }
