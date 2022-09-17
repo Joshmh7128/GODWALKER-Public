4fbb1a63e3cea57c6c702afb8c4cc187372d7771
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-09-17 15:00:59 -0400

    adding update ui info section

--- Assets/Scripts/Player/PlayerUIManager.cs
+++ Assets/Scripts/Player/PlayerUIManager.cs
@@ -18,9 +18,62 @@ public class PlayerUIManager : MonoBehaviour
     /// 5 - left leg
     /// </summary>
 
+    // our instance of the bodypart manager 
+    PlayerBodyPartManager partManager;
 
     // lists concerning bodyparts
     [SerializeField] List<Text> nameDisplays;
     [SerializeField] List<Text> infoDisplays;
+
+    // ui elements
+    [SerializeField] CanvasGroup bodyPartCanvasGroup; // the body part canvas group we'll be interacting with
+
+    // start
+    private void Start()
+    {
+        // set the instance of this
+        partManager = PlayerBodyPartManager.instance;
+    }
+
+    private void FixedUpdate()
+    {
+        // all things we need to process related to our input
+        ProcessInput();
+    }
+
+
+    // set our names and info
+    private void UpdateUI()
+    {
+
+    }
+
+    // process our inputs
+    private void ProcessInput()
+    {
+        // whenever we press tab, show the entire body part canvas
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            ShowCanvas();
+        } else
+        {
+            HideCanvas();
+        }
+    }
+
+    // show our canvas
+    private void ShowCanvas()
+    {
+        bodyPartCanvasGroup.alpha = 1;
+    }
+
+    // fading out our canvas
+    private void HideCanvas()
+    {
+        if (bodyPartCanvasGroup.alpha > 0)
+        {
+            bodyPartCanvasGroup.alpha -= 0.05f;
+        }
+    }
 
 }
