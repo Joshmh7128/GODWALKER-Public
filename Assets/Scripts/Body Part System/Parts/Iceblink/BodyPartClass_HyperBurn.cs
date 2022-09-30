616d5182bc7a5e33367d80599bae922298f2dead
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-09-30 17:51:27 -0400

    building Iceblink Hyperburn

--- Assets/Scripts/Body Part System/Parts/Iceblink/BodyPartClass_HyperBurn.cs
+++ Assets/Scripts/Body Part System/Parts/Iceblink/BodyPartClass_HyperBurn.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BodyPartClass_IceblinkOnset : BodyPartClass
+public class BodyPartClass_HyperBurn : BodyPartClass
 {
     // All shots are now homing, lol
+    /// <summary>
+    /// - All bullets now Explode
+    /// - Whenever an Explosion deals damage to multiple targets, your next shot is a Double Shot
+    /// - All Double Shots and Homing Shots Explode
+    /// - Whenever you are damaged by an Explosion, fire 20 Life-steal Homing Shots
+    /// </summary>
+
     PlayerWeaponManager weaponManager; // our weapon manager
 
     public override void PartStart()
