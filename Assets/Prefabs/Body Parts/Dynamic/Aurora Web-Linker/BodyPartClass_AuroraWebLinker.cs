bb846b87aee62bd2b428c06e455f2017f7ba42c5
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-10-06 08:55:54 -0400

    work started on aurora web linker

---
+++ Assets/Prefabs/Body Parts/Dynamic/Aurora Web-Linker/BodyPartClass_AuroraWebLinker.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartClass_AuroraWebLinker : BodyPartClass
+{
+    ///
+    /// - All shots deal Shock damage
+    /// - Pressing Ability Button Tethers together all Shocked enemies 
+    /// - Tethers deal Shock damage to Enemies
+    /// - Damage is now tied to Enemies Tethered
+    /// 
+
+
+}
