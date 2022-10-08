1882f68c78f98cac51855a4bde6f73c25b992f8f
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-10-07 23:05:57 -0400

    working on helion enemy

---
+++ Assets/Scripts/Enemies/EnemyClass_Helion.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyClass_Helion : EnemyClass
+{
+    /// the helion is a huge enemy that fires noisy shots out of the tips all over its body
+    /// it alternates between noise mode (orange) and homing mode (purple)
+    [SerializeField] float baseHealth; // health we set in-editor, this is a beefier enemy so we want it to be higher
+
+    // set our stats
+    public override void SetLevelStats()
+    {
+        // set our health
+        maxHealth = (level + level * 0.15f) * baseHealth; // crabs have 100hp per level
+        health = maxHealth;
+        // set our damage
+        // damage = level * (2 + level * 0.1f); // this is a standard curve
+        damage = 10; // set our basic damage
+    }
+
+
+
+}
