f0d2288f839cd599ffc115ec745a070295c1942d
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-11-27 10:19:28 -0500

    adding auto pickups

--- Assets/Scripts/Environment/HealthRestore_Item.cs
+++ Assets/Scripts/Environment/HealthRestore_Item.cs
@@ -8,6 +8,8 @@ public class HealthRestore_Item : ItemClass
     string infoString; // our info
     [SerializeField] ItemUIHandler uiHandler; // our UI handler
     [SerializeField] GameObject vfx; // our vfx
+    [SerializeField] bool autoPickup; // does the player pick this up by walking over it?
+    [SerializeField] float autoPickupDistance; // how far from the player is this picked up?
 
     // our instance
     PlayerStatManager playerStatManager;
@@ -41,16 +43,34 @@ public class HealthRestore_Item : ItemClass
     void ProcessPickup()
     {
         // if we can grab and can pickup
-        if (canGrab & Input.GetKeyDown(KeyCode.E))
+        if (!autoPickup)
         {
-            Debug.Log("adding " + healthRestore);
-            playerStatManager.AddHealth(healthRestore);
-            // spawn fx on player
-            Instantiate(vfx, PlayerController.instance.transform.position + Vector3.up * 0.5f, vfx.transform.rotation);
-            // then destroy object
-            Destroy(uiHandler.gameObject);
-            Destroy(gameObject);
+            if (canGrab & Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("adding " + healthRestore);
+                playerStatManager.AddHealth(healthRestore);
+                // spawn fx on player
+                Instantiate(vfx, PlayerController.instance.transform.position + Vector3.up * 0.5f, vfx.transform.rotation);
+                // then destroy object
+                Destroy(uiHandler.gameObject);
+                Destroy(gameObject);
+            }
         }
+
+        if (autoPickup)
+        {
+            if (Vector3.Distance(transform.position, playerStatManager.transform.position) < autoPickupDistance)
+            {
+                Debug.Log("adding " + healthRestore);
+                playerStatManager.AddHealth(healthRestore);
+                // spawn fx on player
+                Instantiate(vfx, PlayerController.instance.transform.position + Vector3.up * 0.5f, vfx.transform.rotation);
+                // then destroy object
+                Destroy(uiHandler.gameObject);
+                Destroy(gameObject);
+            }
+        }
+
 
 
     }
