628d5a81ce23297891d6293e7acd86381bddfe3a
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-12-03 13:01:59 -0500

    weapon recharges work

--- Assets/Scripts/UI Handlers/WeaponChargeUIHandler.cs
+++ Assets/Scripts/UI Handlers/WeaponChargeUIHandler.cs
@@ -8,11 +8,16 @@ public class WeaponChargeUIHandler : MonoBehaviour
 {
     // used to access and show charge of weapons
     public int weaponID; // which weapon are we working with? set when created
-    public TextMeshProUGUI weaponNameDisplay; // the name of our weapon to display. set when created
+    public TextMeshProUGUI weaponNameDisplay; // the name of our weapon to display. set in here
     public Slider chargeSlider; // displays charge, set in here
     public WeaponClass ourWeapon; // the weapon we're working with, set on creation
     public Image chargeImage; // the image of our charge that we're going to change the color of
     public Color charging, idle; // the idle and charging colors
+
+    private void Start()
+    {
+        weaponNameDisplay.text = ourWeapon.weaponName;
+    }
 
     private void FixedUpdate()
     {
@@ -26,12 +31,12 @@ public class WeaponChargeUIHandler : MonoBehaviour
         if (ourWeapon.gameObject.activeInHierarchy == false)
         {
             chargeImage.color = charging;   
-            chargeSlider.value = ourWeapon.recharge / ourWeapon.rechargeMax;
+            chargeSlider.value = ourWeapon.currentMagazine / ourWeapon.maxMagazine;
         }
         else
         {
             chargeImage.color = idle;
-            chargeSlider.value = ourWeapon.recharge / ourWeapon.rechargeMax;
+            chargeSlider.value = ourWeapon.currentMagazine / ourWeapon.maxMagazine;
         }
     }
 
