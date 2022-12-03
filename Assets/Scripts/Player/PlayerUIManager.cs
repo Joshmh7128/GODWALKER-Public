a3b2d6e0b2b455d3843bb62aca2de44b93f23ab2
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-12-03 12:41:35 -0500

    stable again, fixed UI

--- Assets/Scripts/Player/PlayerUIManager.cs
+++ Assets/Scripts/Player/PlayerUIManager.cs
@@ -80,62 +80,6 @@ public class PlayerUIManager : MonoBehaviour
         UpdateAbilityUI(true);        
     }
 
-    // set our names and info
-    private void UpdateBodyPartUI()
-    {
-        // set all names and info to the correct text
-        for (int i = 0; i < nameDisplays.Count; i++)
-        {
-            nameDisplays[i].text = partManager.bodyParts[i].bodyPartName; // set the name
-            infoDisplays[i].text = partManager.bodyParts[i].descriptiveInfo; // set the descriptive information
-
-            // set the correlating background display to the average color of the bodypart
-            Color panelColor = Color.white;
-            foreach (GameObject cos in partManager.bodyParts[i].cosmeticParts)
-            {
-                Color cosColor = Color.white;
-                Color temp = Color.white;
-                // get the average color of the parts in the cosmeticpart parent
-                foreach (Transform child in cos.transform)
-                {
-                    if (child.GetComponent<Renderer>() != null)
-                    {
-
-                        // set our temp to the color
-                        temp.r = child.gameObject.GetComponent<Renderer>().sharedMaterial.color.r;
-                        temp.g = child.gameObject.GetComponent<Renderer>().sharedMaterial.color.g;
-                        temp.b = child.gameObject.GetComponent<Renderer>().sharedMaterial.color.b;
-                        // lerp the color
-                        cosColor = Color.Lerp(cosColor, temp, 0.5f);
-                        cosColor = Color.Lerp(cosColor, Color.white, 0.25f); // lerp back to white for visibility
-                    }
-                }
-
-                // get the color and lerp it
-                panelColor = Color.Lerp(panelColor, cosColor, 0.5f);
-            }
-            // once we lerp to all the colors set the panel color
-            backgroundPanels[i].color = panelColor;
-
-        }
-    }
-
-    // update our weapon info
-    void UpdateWeaponInfoUI()
-    {
-        WeaponClass weapon_Class = weaponManager.currentWeapon;
-
-        // set the info for our player
-        string weaponInfo =
-             "Damage: " + weapon_Class.damage + "\n" +
-             weapon_Class.customInfo;
-
-        infoText.text = weaponInfo;
-        nameText.text = weapon_Class.weaponName;
-        lvlText.text = "Lvl " + weapon_Class.level.ToString();
-
-
-    }
 
     // process our inputs
     private void ProcessInput()
@@ -154,9 +98,6 @@ public class PlayerUIManager : MonoBehaviour
     private void ShowCanvas()
     {
         infoCanvasGroup.alpha = 1;
-        // update our information whenever we show it
-        UpdateBodyPartUI();
-        UpdateWeaponInfoUI();
     }
 
     // fading out our canvas
