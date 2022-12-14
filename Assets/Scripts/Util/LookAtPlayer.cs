fff6f968f2d0bcd475567ad18f42b8743a74b46a
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-12-13 19:32:10 -0500

    look at player util script updated for slow movmenet

--- Assets/Scripts/Util/LookAtPlayer.cs
+++ Assets/Scripts/Util/LookAtPlayer.cs
@@ -4,14 +4,48 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
-    [SerializeField] bool lookAtCamera; // do we look at the camera?
+    [SerializeField] bool lookAtCamera, slowMove; // do we look at the camera? do we move slow?
+    [SerializeField] float turnSpeed; // if we are moving slow then use this
+
+    // instance
+    PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = PlayerController.instance;
+    }
 
     private void FixedUpdate()
     {
-        if (!lookAtCamera)
-            transform.LookAtPlayer();
+        if (!slowMove)
+        {
+            if (!lookAtCamera)
+                transform.LookAtPlayer();
 
-        if (lookAtCamera)
-            transform.LookAtCamera();
+            if (lookAtCamera)
+                transform.LookAtCamera();
+        }
+
+        // if we are slow moving
+        if (slowMove)
+        {
+            if (!lookAtCamera)
+            { 
+                // get the look direction
+                Vector3 lookDir = transform.position - playerController.transform.position;
+
+                // slerp to the look direction
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDir), turnSpeed * Time.fixedDeltaTime);
+            }
+
+            if (lookAtCamera)
+            { 
+                // get the look direction
+                Vector3 lookDir = transform.position - playerController.cameraRig.transform.position;
+
+                // slerp to the look direction
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDir), turnSpeed * Time.fixedDeltaTime);
+            }
+        }
     }
 }
