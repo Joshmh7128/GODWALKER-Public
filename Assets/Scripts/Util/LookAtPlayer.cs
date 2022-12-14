4394d4ac9beb38a00ef51e293e24f5b8fc2a5045
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-12-13 20:13:54 -0500

    adding distance compensation

--- Assets/Scripts/Util/LookAtPlayer.cs
+++ Assets/Scripts/Util/LookAtPlayer.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class LookAtPlayer : MonoBehaviour
 {
     [SerializeField] bool lookAtCamera, slowMove; // do we look at the camera? do we move slow?
-    [SerializeField] float turnSpeed; // if we are moving slow then use this
+    [SerializeField] float turnSpeed, minDistance, maxDistance; // if we are moving slow then use this
+    [SerializeField] bool distanceCompensation; // move at different speeds depending on player distance
 
     // instance
     PlayerController playerController;
@@ -33,18 +34,39 @@ public class LookAtPlayer : MonoBehaviour
             { 
                 // get the look direction
                 Vector3 lookDir = playerController.transform.position - transform.position;
+                float finalSpeed = turnSpeed; 
+
+                // calculate our distance compensation
+                if (distanceCompensation)
+                {
+                    // the further the player is the slower we move
+                    if (Vector3.Distance(playerController.transform.position, transform.position) > maxDistance)
+                    {
+                        finalSpeed /= 2;
+                    }
+                }
 
                 // slerp to the look direction
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDir), turnSpeed);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDir), finalSpeed * Time.fixedDeltaTime);
             }
 
             if (lookAtCamera)
             { 
                 // get the look direction
                 Vector3 lookDir = playerController.cameraRig.transform.position - transform.position;
+                float finalSpeed = turnSpeed;
 
+                // calculate our distance compensation
+                if (distanceCompensation)
+                {
+                    // the further the player is the slower we move
+                    if (Vector3.Distance(playerController.transform.position, transform.position) > maxDistance)
+                    {
+                        finalSpeed /= 2;
+                    }
+                }
                 // slerp to the look direction
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDir), turnSpeed);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDir), finalSpeed * Time.fixedDeltaTime);
             }
         }
     }
