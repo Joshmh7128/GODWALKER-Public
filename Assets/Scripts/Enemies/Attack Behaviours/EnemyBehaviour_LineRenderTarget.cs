676994b691cd8d795d3856ba8eb97f2646fed637
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-12-13 19:42:17 -0500

    updating target behaviour

--- Assets/Scripts/Enemies/Attack Behaviours/EnemyBehaviour_LineRenderTarget.cs
+++ Assets/Scripts/Enemies/Attack Behaviours/EnemyBehaviour_LineRenderTarget.cs
@@ -11,13 +11,23 @@ public class EnemyBehaviour_LineRenderTarget : EnemyBehaviour
     Vector3 targetPos; // where is this line renderer going?
     [SerializeField] Color startColor, endColor; // our start and end colors
     bool running = false; // we aren't running yet
-
-
+    [SerializeField] Transform alignmentTransform; // the transform we base our forward off of
+    float timePassed;
+    [SerializeField] float startSize, endSize; // the start and end size of our line
     private void FixedUpdate()
     {
         if (running)
         {
+            // continue time passed
+            timePassed += Time.fixedDeltaTime;
             // set our line renderer positions
+            targetPos = alignmentTransform.forward * 100f;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, targetPos);
+            // lerp our line renderer colors
+            Color lerpColor = Color.Lerp(startColor, endColor, timePassed / behaviourTime);
+            lineRenderer.startColor = lerpColor;
+            lineRenderer.endColor = lerpColor;
 
         }
     }
@@ -27,6 +37,8 @@ public class EnemyBehaviour_LineRenderTarget : EnemyBehaviour
         // set our second position of the line renderer
         running = true; // we have started running
 
+        yield return new WaitForSeconds(behaviourTime);
+        complete = true;
         yield return null; 
     }
 }
