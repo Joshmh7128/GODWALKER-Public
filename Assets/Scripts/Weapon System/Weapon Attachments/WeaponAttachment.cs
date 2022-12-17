3f7cb13122cd703fea73a8641fe0dc6c59b307e2
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-12-17 13:03:31 -0500

    building shotgun charge attachment

--- Assets/Scripts/Weapon System/Weapon Attachments/WeaponAttachment.cs
+++ Assets/Scripts/Weapon System/Weapon Attachments/WeaponAttachment.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponAttachment : MonoBehaviour
+public abstract class WeaponAttachment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// this is the paretn class for all weapon attachments
+    /// it holds basic info that we can use to interact with weapons through attachments
+    /// 
+
+    public WeaponClass weaponClass; // the weapon class we're interacting with
+
+    public void Update()
     {
+        InputCollection();
+    }
+
+    // where we get all our inputs
+    void InputCollection()
+    {
+        // mouse
+        if (Input.GetMouseButtonDown(0)) MouseButtonDown0();
+        if (Input.GetMouseButtonDown(1)) MouseButtonDown1();
+        if (Input.GetMouseButtonUp(0)) MouseButtonUp0();
+        if (Input.GetMouseButtonUp(1)) MouseButtonUp1();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // our mouse inputs
+    public abstract void MouseButtonDown0(); 
+    public abstract void MouseButtonDown1(); 
+    public abstract void MouseButtonUp0(); 
+    public abstract void MouseButtonUp1(); 
 }
