9bb07269b721fdf5f9e49fbe2024c52dd431eb50
Josh <joshmh7128@gmail.com>
2023-01-26 15:34:29 -0500

    attempting to make the transiton more seamless

--- Assets/Scripts/Environment/TweenRoomHandler.cs
+++ Assets/Scripts/Environment/TweenRoomHandler.cs
@@ -13,9 +13,11 @@ public class TweenRoomHandler : MonoBehaviour
     bool used; // has this been used?
 
     [SerializeField] GameObject backDoor, frontDoor; // our doors
+    Vector3 frontDoorMove; // where we want our front door to move
 
     private void Start()
     {
+        frontDoorMove = frontDoor.transform.position;
         // make this do not destroy
         DontDestroyOnLoad(gameObject);
     }
@@ -32,6 +34,11 @@ public class TweenRoomHandler : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        frontDoor.transform.position = Vector3.MoveTowards(frontDoor.transform.position, frontDoorMove, 50 * Time.fixedDeltaTime);
+    }
+
     void MoveToNewScene()
     {
         // close back door
@@ -43,9 +50,10 @@ public class TweenRoomHandler : MonoBehaviour
         // we always start a 0, 0, so go there
         transform.position = Vector3.zero;
         // move to the player to our position minus their position
-        PlayerController.instance.Teleport(Vector3.zero - playerDif);
+        PlayerController.instance.Teleport(Vector3.zero - playerDif - Vector3.up); // we subtract up from this to teleport to the EXACT location they are standing
         // open front door
-        frontDoor.SetActive(false);
+        frontDoorMove = frontDoor.transform.position;
+        frontDoorMove = frontDoorMove - new Vector3(0, -50, 0);
     }
 
 }
