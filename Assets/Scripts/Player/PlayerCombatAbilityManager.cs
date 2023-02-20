8e063898045c05f9e10b77353d553c251d998cbc
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-02-20 11:20:05 -0500

    starting work on player combat abilities

---
+++ Assets/Scripts/Player/PlayerCombatAbilityManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatAbilityManager : MonoBehaviour
+{
+    // script is used to manage the combat abilities, which keys they are bound to
+
+}
