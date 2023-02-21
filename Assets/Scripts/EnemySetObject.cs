d952f2057bff656d273af600d0736e4583f409bd
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-02-21 13:28:19 -0500

    Working on torrent

--- Assets/Scripts/EnemySetObject.cs
+++ Assets/Scripts/EnemySetObject.cs
@@ -4,6 +4,16 @@ using UnityEditor;
 using UnityEngine;
 
 
+/// <summary>
+/// Some personal notes about enemy encounters:
+/// Room 1: Basic crabs and orbs
+/// Room 2: Basic crabs, orbs, and missile launchers
+/// Room 3: Basic crabs, orbs, missile launchers, centaurs
+/// Room 4: Crabs, Orbs, missiles, centaurs, torrents
+/// Room 5: crabs, orbs, missiles, centaurs, torrents, helion
+/// Room 6: crabs, orbs, missiles, centaurs, torrents, helion, boom crabs
+/// </summary>
+
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/EnemySetObject", order = 1)]
 public class EnemySetObject : ScriptableObject
 {
