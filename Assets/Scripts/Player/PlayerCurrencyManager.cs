08ad252407a660d36f76555173bb60fc744bfcda
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-02-28 20:03:58 -0500

    starting work on currency canvas

---
+++ Assets/Scripts/Player/PlayerCurrencyManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerCurrencyManager : MonoBehaviour
+{
+    /// 
+    /// this script serves to manage the player's currency balance
+    /// there is no maximum to the player's currency. 
+    /// 
+
+    public int playerCurrencyAmount; // how much currency do we currently have?
+
+    [SerializeField] TextMeshProUGUI currencyDisplayAmount;
+
+    // adding currency
+    public void AddCurrency(int amount)
+    {
+        playerCurrencyAmount += amount;
+
+        // after adding currency update the UI
+        UpdateUI();
+    }
+
+    // subtracting currency
+    public void SubCurrency(int amount)
+    {
+        if (playerCurrencyAmount - amount >= 0)
+            playerCurrencyAmount -= amount;
+
+        // after subtracting currency update the UI
+        UpdateUI();
+    }
+
+    // run our UI
+    void UpdateUI()
+    {
+        currencyDisplayAmount.text = playerCurrencyAmount.ToString();
+    }
+
+
+
+}
