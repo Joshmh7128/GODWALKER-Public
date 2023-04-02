997f5ad3f39cbd0cc9553b5f7ca08697e8bae5ae
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-04-02 12:32:14 -0400

    Balancing the ballista a bit more, added the main menu and main menu manager, added in another font for the main menu buttons

--- Assets/Scripts/Util/MainMenuManager.cs
+++ Assets/Scripts/Util/MainMenuManager.cs
@@ -3,9 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI aimSensitivityText;
+    [SerializeField] Slider aimSenseSlider; 
+
+    float aimSensitivity;
+
+    private void Start()
+    {
+        PlayerPrefs.GetFloat("sensitivity", aimSensitivity);
+        // set our sensitivity slider to the last used sense
+        if (aimSensitivity != 0)
+        {
+            aimSenseSlider.value = aimSensitivity;
+        }
+        else
+        {
+            aimSensitivity = 15;
+            aimSenseSlider.value = aimSensitivity;
+        }
+    }
+
+    private void Update()
+    {
+        SetSensitivity();
+    }
+
+    public void SetSensitivity()
+    {
+        PlayerPrefs.GetFloat("sensitivity", aimSensitivity);
+        aimSensitivityText.text = "Sensitivity Slider. Current Sensitivity = " + aimSensitivity;
+        aimSensitivity = aimSenseSlider.value;
+
+        PlayerPrefs.SetFloat("sensitivity", aimSensitivity);
+
+    }
+
     // load the game
     public void LoadGame()
     {
